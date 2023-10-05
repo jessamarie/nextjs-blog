c6d8aa993f1a1c916964305e1a3205746c5320ae
Jessamarie <jessamarie@users.noreply.github.com>
2023-10-04 20:44:42 -0400

    update basePath for github pages

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+    };
+  }
+
+  return {
+    basePath: '/nextjs-blog'
+  };
+};
